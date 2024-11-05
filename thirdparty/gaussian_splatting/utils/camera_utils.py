c75741de430b4c577d8cf236f9ae8b404dd27faa
Woongoh Cho <whdnddh@naver.com>
2024-11-05 16:38:36 +0900

    fix

--- thirdparty/gaussian_splatting/utils/camera_utils.py
+++ thirdparty/gaussian_splatting/utils/camera_utils.py
@@ -21,8 +21,11 @@ WARNED = False
 
 
 
-def loadCamv2(args, id, cam_info, resolution_scale):
-    orig_w, orig_h = cam_info.image.size
+def loadCamv2(args, id, cam_info, resolution_scale, nogt=False):
+    if not nogt:
+        orig_w, orig_h = cam_info.image.size
+    else:
+        orig_w, orig_h = cam_info.width, cam_info.height
 
     if args.resolution in [1, 2, 4, 8]:
         resolution = round(orig_w/(resolution_scale * args.resolution)), round(orig_h/(resolution_scale * args.resolution))
@@ -43,26 +46,44 @@ def loadCamv2(args, id, cam_info, resolution_scale):
         scale = float(global_down) * float(resolution_scale)
         resolution = (int(orig_w / scale), int(orig_h / scale))
 
-    resized_image_rgb = PILtoTorch(cam_info.image, resolution)
-
-    gt_image = resized_image_rgb[:3, ...]
-    loaded_mask = None
-
-    if resized_image_rgb.shape[1] == 4:
-        loaded_mask = resized_image_rgb[3:4, ...]
-
+    resolution = (int(orig_w / 2), int(orig_h / 2))
     cameradirect = cam_info.hpdirecitons
     camerapose = cam_info.pose 
-     
-    if camerapose is not None:
-        rays_o, rays_d = 1, cameradirect
-    else :
-        rays_o = None
-        rays_d = None
-    return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
-                  FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
-                  image=gt_image, gt_alpha_mask=loaded_mask,
-                  image_name=cam_info.image_name, uid=id, data_device=args.data_device, near=cam_info.near, far=cam_info.far, timestamp=cam_info.timestamp, rayo=rays_o, rayd=rays_d,cxr=cam_info.cxr,cyr=cam_info.cyr)
+    loaded_mask = None
+
+    # load gt image 
+    if nogt == False :
+        resized_image_rgb = PILtoTorch(cam_info.image, resolution)
+
+        gt_image = resized_image_rgb[:3, ...]
+        loaded_mask = None
+
+        if resized_image_rgb.shape[1] == 4:
+            loaded_mask = resized_image_rgb[3:4, ...]
+
+        cameradirect = cam_info.hpdirecitons
+        camerapose = cam_info.pose 
+        
+        if camerapose is not None:
+            rays_o, rays_d = 1, cameradirect
+        else :
+            rays_o = None
+            rays_d = None
+        return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
+                    FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
+                    image=gt_image, gt_alpha_mask=loaded_mask,
+                    image_name=cam_info.image_name, uid=id, data_device=args.data_device, near=cam_info.near, far=cam_info.far, timestamp=cam_info.timestamp, rayo=rays_o, rayd=rays_d,cxr=cam_info.cxr,cyr=cam_info.cyr)
+    else:
+        if camerapose is not None:
+            rays_o, rays_d = 1, cameradirect
+        else :
+            rays_o = None
+            rays_d = None
+        return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
+                    FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
+                    image=gt_image, gt_alpha_mask=loaded_mask,
+                    image_name=cam_info.image_name, uid=id, data_device=args.data_device, near=cam_info.near, far=cam_info.far, timestamp=cam_info.timestamp, rayo=rays_o, rayd=rays_d,cxr=cam_info.cxr,cyr=cam_info.cyr)
+
 
 
 
@@ -239,7 +260,7 @@ def cameraList_from_camInfosv2(cam_infos, resolution_scale, args, ss=False):
 
     if not ss: #
         for id, c in enumerate(cam_infos):
-            camera_list.append(loadCamv2(args, id, c, resolution_scale))
+            camera_list.append(loadCamv2(args, id, c, resolution_scale, nogt=True))
     else:
         for id, c in enumerate(cam_infos):
             camera_list.append(loadCamv2ss(args, id, c, resolution_scale, nogt=True))
